3052f84c719f0f5d8c16a7fdbfafc852c7aa4013
S0DIENYE <peppledavid29@gmail.com>
2023-06-26 14:56:55 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+// Send data with end point
+/**
+ * res.send('takes in text');
+ * res.json({msg: 'takes in data with json format'}),
+ */
+app.get('/', (req, res) => res.json({msg: 'Welcome to contact keeper API'}) );
+
+// Define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contact'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
