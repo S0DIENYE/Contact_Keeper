af6fcb994a3ede67f026f3c00deeff5bf0386f97
S0DIENYE <peppledavid29@gmail.com>
2023-06-26 15:03:16 +0100

    express server setup with initial routes

--- routes/contact.js
+++ routes/contact.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
 // @desc    Add new contact
 // @access  Private
 router.post('/', (req, res) => {
-    res.send('Add new contacts');
+    res.send('Add new contacts'); 
 });
 
 // @route   POST api/contacts:id
